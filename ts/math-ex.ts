61a63ceadb706be2b8319ac50ba4aeb8d0b9a87b
CJ Dimaano <c.j.s.dimaano@gmail.com>
2017-07-19 06:54:46 -0700

    add rotation functions

--- ts/math-ex.ts
+++ ts/math-ex.ts
@@ -38,6 +38,22 @@ export function translate(v: vec2, u: vec2): vec2 {
 
 /**
  * @summary
+ *   Produces a new vector rotated by the given vector and radians.
+ *
+ * @param v
+ *   The vector to be rotated.
+ *
+ * @param rad
+ *   The number of radians to rotate the vector.
+ */
+export function rotate(v: vec2, rad: number): vec2 {
+  const cos = Math.cos(rad)
+  const sin = Math.sin(rad)
+  return [v[0] * cos - v[1] * sin, v[0] * sin + v[1] * cos]
+}
+
+/**
+ * @summary
  *   Scales the components of a vector by the given factor. Scaling is performed
  *   in-place.
  * 
@@ -66,4 +82,24 @@ export function _scale(v: vec2, factor: number): void {
 export function _translate(v: vec2, u: vec2): void {
   v[0] += u[0]
   v[1] += u[1]
+}
+
+/**
+ * @summary
+ *   Rotates the given vector by the given number of radians. Rotation is
+ *   performed in-place.
+ *
+ * @param v
+ *   The vector to be rotated.
+ *
+ * @param rad
+ *   The number of radians to rotate the vector.
+ */
+export function _rotate(v: vec2, rad: number): void {
+  const cos = Math.cos(rad)
+  const sin = Math.sin(rad)
+  const v0 = v[0]
+  const v1 = v[1]
+  v[0] = v0 * cos - v1 * sin
+  v[1] = v0 * sin + v1 * cos
 }
