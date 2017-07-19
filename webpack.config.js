e8bf93858e6198ca64378fb4913857a22ee3e339
CJ Dimaano <c.j.s.dimaano@gmail.com>
2017-07-19 15:58:38 -0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+@file `webpack.config.js`
+  Created July 19, 2017
+
+@author CJ Dimaano
+  <c.j.s.dimaano@gmail.com>
+*******************************************************************************/
+
+module.exports = {
+  entry: {
+    index: "./ts/index"
+  },
+  output: {
+    filename: "index.js"
+  },
+  node: {
+    __dirname: false
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: "ts-loader" }
+    ]
+  }
+}
