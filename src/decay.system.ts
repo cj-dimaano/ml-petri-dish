f9ac1cd0ea10703aa2b10699967f2b53b3d4bbc2
CJ Dimaano <c.j.s.dimaano@gmail.com>
2017-07-19 21:30:11 -0700

    add decay system

---
+++ src/decay.system.ts
@@ -0,0 +1,28 @@
+/*******************************************************************************
+@file `decay.system.ts`
+  Created July 19, 2017
+
+@author CJ Dimaano
+  <c.j.s.dimaano@gmail.com>
+*******************************************************************************/
+
+import { GameComponentSystem } from "./game-system";
+import { GameEntity } from "./game-entity";
+import { DecayComponent, GameComponentKinds } from "./components";
+
+export class DecaySystem extends GameComponentSystem {
+  constructor() { super(GameComponentKinds.Decay) }
+  protected createComponent(host: GameEntity): DecayComponent {
+    const result = new DecayComponent(host)
+    return result
+  }
+  update(dt: number): void {
+    this.components.forEach(
+      (value) => {
+        const decay = <DecayComponent>value
+        if (Math.random() < decay.calculateProbability(dt))
+          decay.host.dispose()
+      }
+    )
+  }
+}
