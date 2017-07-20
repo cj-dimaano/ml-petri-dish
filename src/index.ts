97bdab3fd74c848d97e6b750e921aa3e1442a331
CJ Dimaano <c.j.s.dimaano@gmail.com>
2017-07-20 16:12:14 -0700

    scrape unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@
   <c.j.s.dimaano@gmail.com>
 *******************************************************************************/
 
-import { ParticleSystem } from "./particle.system"
-import { DecaySystem } from "./decay.system"
 import { FiatSystem } from "./fiat.system"
 
 let canvas: HTMLCanvasElement
@@ -16,13 +14,9 @@ let screenHeight: number
 let screenWidth: number
 let prevTimestamp: number
 
-let particleSystem: ParticleSystem
-let decaySystem: DecaySystem
 let fiatSystem: FiatSystem
 
 function update(dt: number) {
-  decaySystem.update(dt)
-  particleSystem.update(dt)
   fiatSystem.update(dt)
 }
 
@@ -49,9 +43,7 @@ function init() {
   g = <CanvasRenderingContext2D>canvas.getContext("2d")
   screenHeight = canvas.clientHeight
   screenWidth = canvas.clientWidth
-  particleSystem = new ParticleSystem(screenHeight, screenWidth)
-  decaySystem = new DecaySystem()
-  fiatSystem = new FiatSystem(particleSystem, decaySystem)
+  fiatSystem = new FiatSystem(screenHeight, screenWidth)
   window.requestAnimationFrame(updateAndDraw)
 }
 
