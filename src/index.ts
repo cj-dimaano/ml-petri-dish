17fd5dbbf0e16c585513bba736fa0fbcaabd4b10
CJ Dimaano <c.j.s.dimaano@gmail.com>
2019-09-03 14:13:28 -0700

    add duration

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@
 *******************************************************************************/
 
 import Game from "./game";
+import * as moment from "moment";
 
 window.addEventListener("load", () => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const game = new Game(canvas.getContext("2d")!);
+    const durationElement = document.getElementById("duration")!;
     let prevTick = performance.now();
+    const start = prevTick;
     const animation = (now: DOMHighResTimeStamp) => {
+        durationElement.textContent = moment.duration(start - now).humanize();
         game.update((now - prevTick) / 1000.0);
         game.draw();
         prevTick = now;
