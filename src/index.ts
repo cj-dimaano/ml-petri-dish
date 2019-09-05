ad6cb35cd86dfd02b40855b9d7c12fe8f920d069
CJ Dimaano <c.j.s.dimaano@gmail.com>
2019-09-05 16:13:17 -0700

    implement deep double-q learning

--- src/index.ts
+++ src/index.ts
@@ -5,83 +5,131 @@
 
 import Game from "./game";
 import * as moment from "moment";
-
-import ANN from "./artificial-neural-network";
 import * as numeral from "numeral";
 
-(() => {
+import * as LA from "./linear-algebra";
+import ANN from "./artificial-neural-network";
 
-    window.addEventListener("load", () => {
-        const MAX_DIM = 30;
-        const getState = (v: number[], u: number[], a: number) => [...v, ...u, a];
-        const clip = (val: number, min: number, max: number) => Math.min(Math.max(val, min), max);
-        const grid = document.getElementById("grid")! as HTMLTableElement;
-        const episodeElement = document.getElementById("episode")!;
-        const durationElement = document.getElementById("duration")!;
-        const tdChoices = Array(6);
+(() => {
+    // [state, choice, reward]
+    type memory = [number[], number, number];
+
+    const MAX_DIM = 30;
+
+
+    const dom = new Map<string, HTMLElement>();
+    function saveDomElements(...ids: string[]) {
+        ids.forEach(id => dom.set(id, document.getElementById(id)!));
+    }
+
+    const tdChoices = Array(6);
+    function saveChoicesDomElements() {
         for (let i = 0; i < tdChoices.length; i++)
             tdChoices[i] = document.getElementById(`choice${i}`)!;
-        setupGrid(grid);
+    }
 
-
-        function setupGrid(grid: HTMLTableElement) {
-            for (let r = 0; r < MAX_DIM; r++) {
-                const tr = document.createElement("tr");
-                for (let c = 0; c < MAX_DIM; c++) {
-                    const td = document.createElement("td");
-                    td.id = `r${r}c${c}`;
-                    tr.append(td);
-                }
-                grid.append(tr);
+    function setupGrid(grid: HTMLElement) {
+        for (let r = 0; r < MAX_DIM; r++) {
+            const tr = document.createElement("tr");
+            for (let c = 0; c < MAX_DIM; c++) {
+                const td = document.createElement("td");
+                td.id = `r${r}c${c}`;
+                tr.append(td);
             }
+            grid.append(tr);
         }
+    }
 
-        function clearGrid(grid: HTMLTableElement) {
-            for (const tr of grid.children) {
-                for (const td of tr.children) {
-                    td.className = "";
-                }
-            }
+    function clearGrid(grid: HTMLElement) {
+        for (const tr of grid.children) {
+            for (const td of tr.children)
+                td.className = "";
         }
+    }
 
-        function getCell(v: number[]): HTMLElement {
-            return document.getElementById(`r${v[0]}c${v[1]}`)!;
+    function getCell(v: number[]): HTMLElement {
+        return document.getElementById(`r${v[0]}c${v[1]}`)!;
+    }
+
+    function updateTdChoices(Qa: number[], Qb: number[]) {
+        for (let i = 0; i < Qa.length; i++) {
+            // tdChoices[i].textContent
+            //     = `${Qa[i]}, ${Qb[i]}`;
+            tdChoices[i].children[0].textContent = Qa[i];
+            tdChoices[i].children[1].textContent = Qb[i];
         }
+    }
 
-        let episode = 0;
+    function updateDuration(timespan: number) {
+        // dom.get("duration")!.textContent = moment.duration(timespan).humanize();
+        dom.get("duration")!.textContent = numeral(-timespan/1000).format("00:00:00");
+    }
+
+    function getState(v: number[], u: number[], a: number) {
+        return [...v, ...u, a];
+    }
+
+    function clip(val: number, min: number, max: number) {
+        return Math.min(Math.max(val, min), max);
+    }
+
+
+    window.addEventListener("load", () => {
+        saveDomElements("grid", "episode", "duration");
+        saveChoicesDomElements();
+        setupGrid(dom.get("grid")!);
+
+
+        /*
+        # double q-learning
+        https://en.wikipedia.org/wiki/Q-learning#Double_Q-learning
+        https://www.analyticsvidhya.com/blog/2019/04/introduction-deep-q-learning-python/
+        https://towardsdatascience.com/self-learning-ai-agents-part-ii-deep-q-learning-b5ac60c3f47
+        */
         let angle = 0
         let a: number[] = [];
         let b: number[] = [];
-        let state = [];
-        let mem: [number[], number[], number] = [[], [], 0];
-        let steps = 0;
-        const ann = new ANN(5, 6, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
+        // [state_t, action_t, reward_(t+1)]
+        const mem: memory[] = [];
+        // policy a and b
+        const Pa = new ANN(5, 6, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
+        const Pb = new ANN(5, 6, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
 
-        function makeChoice(options: number[]): number {
-            // if (Math.random() < episode / (10 + episode)) {
+
+        function argMax(options: number[]): number {
+            return options.reduce((p, c, i, a) => a[p] < c ? i : p, 0);
+        }
+
+        function roulette(options: number[]): number {
             const sum = options.reduce((p, c) => p + c);
             let roulette = Math.random() * sum;
             return options.findIndex(v => {
                 roulette -= v;
                 return roulette <= 0;
             });
-            // }
-            // else
-            //     return Math.max(...options);
         }
 
-        function updateWeights() {
-            durationElement.textContent =
-                moment.duration(start - performance.now()).humanize();
-            steps++;
+        function makeChoice(Qa: number[], Qb: number[]): number {
+            /*
+            "clipped double-q"
+            https://towardsdatascience.com/double-deep-q-networks-905dd8325412
+            */
+            let a = 0;
+            let b = 0;
 
-            state = getState(a, b, angle);
-            const outputs = ann.generateOutputs(state);
-            const choice = makeChoice(outputs);
-            console.assert(choice >= 0 && choice < outputs.length);
-            for (let i = 0; i < outputs.length; i++)
-                tdChoices[i].textContent = numeral(outputs[i]).format("0.000");
+            if (Math.random() < 10 / (10 + episode)) {
+                /* roulette */
+                a = roulette(Qa);
+                b = roulette(Qb);
+            } else {
+                /* greedy */
+                a = argMax(Qa);
+                b = argMax(Qb);
+            }
+            return Qa[a] < Qb[b] ? a : b;
+        }
 
+        function updateState(choice: number) {
             if (choice === 1 || choice === 4)
                 angle = (angle - 1 + 4) % 4;
             else if (choice === 2 || choice === 5)
@@ -99,31 +147,114 @@ import * as numeral from "numeral";
                 a[1] = clip(a[1], 0, MAX_DIM - 1);
             }
             getCell(a).classList.add("visited", "active");
+        }
 
-            const maxQ = outputs.reduce((p, c) => Math.max(p, c));
-            const t = [...mem[1]];
-            t[mem[2]] = (1 - ann.learningRate) * t[mem[2]]
-                + ann.learningRate * (
-                    (a[0] === b[0] && a[1] === b[1] ? 1 : 0) /* reward for step */
-                    + (episode / (1 + episode)) /* discount factor (reward decay) */
-                    * maxQ
-                );
-            ann.updateWeights(mem[0], t);
 
-            mem[0] = state;
-            mem[1] = outputs;
-            mem[2] = choice;
+        let memBatch: number = 0;
+        let dream: memory[] = [];
+        function updateWeights() {
+            if (memBatch < 20) {
+                if (dream.length > 1) {
+                    // https://en.wikipedia.org/wiki/Q-learning#Double_Q-learning
+                    const m = dream.shift()!;
+                    const r = dream[0][2];
+                    const nextState = dream[0][0];
+
+                    const Qa = Pa.generateOutputs(m[0]);
+                    const Qb = Pb.generateOutputs(m[0]);
+
+                    const mChoiceA = Qa[m[1]];
+                    const mChoiceB = Qb[m[1]];
+
+                    const discountFactor = 0.95;
+
+                    Qa[m[1]]
+                        = mChoiceA
+                        + Pa.learningRate
+                        * (
+                            r
+                            + discountFactor
+                            * argMax(Pb.generateOutputs(nextState))
+                            - mChoiceA
+                        );
+                    Qb[m[1]]
+                        = mChoiceB
+                        + Pb.learningRate
+                        * (
+                            r
+                            + discountFactor
+                            * argMax(Pa.generateOutputs(nextState))
+                            - mChoiceB
+                        );
+
+                    Pa.updateWeights(m[0], Qa);
+                    Pb.updateWeights(m[0], Qb);
+                } else {
+                    const batchSize = Math.max(mem.length * 0.05, 2);
+                    const memStart = Math.floor(Math.random() * (mem.length - batchSize));
+                    dream = mem.slice(memStart, memStart + batchSize);
+                }
+                memBatch++;
+                setTimeout(() => {
+                    updateDuration(start - performance.now());
+                    updateWeights();
+                }, 0);
+            }
+            else {
+                mem.length = 0;
+                dream.length = 0;
+                setTimeout(() => {
+                    updateDuration(start - performance.now());
+                    updateEpisode();
+                }, 0);
+            }
+        }
+
+
+        let steps = 0;
+        function updateGame() {
+            steps++;
+
+            // get Q predictions
+            const state = getState(a, b, angle);
+            const Qa = Pa.generateOutputs(state);
+            const Qb = Pb.generateOutputs(state);
+
+            // @todo research choice selection techniques
+            const choice = makeChoice(Qa, Qb);
+            // const choice = makeChoice(Qa);
+            console.assert(choice >= 0 && choice < Qa.length);
+            updateTdChoices(Qa, Qb);
+
+            // update angle
+            updateState(choice);
+
+            // save memory
+            mem.push([state, choice, 0]);
 
             if (a[0] === b[0] && a[1] === b[1]) {
                 console.log(steps);
-                setTimeout(updateEpisode, 0);
+                // @todo
+                // use `minSteps / steps` as reward val
+                mem.push([getState(a, b, angle), 0, 1]);
+                memBatch = 0;
+                setTimeout(() => {
+                    updateDuration(start - performance.now());
+                    updateWeights();
+                }, 0);
             }
-            else
-                setTimeout(updateWeights, 0);
+            else {
+                setTimeout(() => {
+                    updateDuration(start - performance.now());
+                    updateGame();
+                }, 0);
+            }
         }
 
+
+        let episode = 0;
         function updateEpisode() {
-            clearGrid(grid)
+            clearGrid(dom.get("grid")!);
             a = [
                 // Math.floor(Math.random() * MAX_DIM),
                 // Math.floor(Math.random() * MAX_DIM)
@@ -134,22 +265,29 @@ import * as numeral from "numeral";
                 // Math.floor(Math.random() * MAX_DIM)
                 MAX_DIM - 1, MAX_DIM - 1
             ];
-            angle = Math.floor(Math.random() * 4); /* north, east, west, south */
-            state = getState(a, b, angle);
-            mem = [state, ann.generateOutputs(state), 0];
+            // angle = Math.floor(Math.random() * 4); /* north, east, west, south */
+            angle = 0;
+            mem.length = 0;
+            mem.push([getState(a, b, angle), 0, 0]);
             steps = 0;
             episode++;
-            episodeElement.textContent = numeral(episode).format("0,0");
+            dom.get("episode")!.textContent = numeral(episode).format("0,0");
 
             // if (episode < 1000) {
             getCell(b).classList.add("destination");
             getCell(a).classList.add("visited", "active");
-            setTimeout(updateWeights, 0);
+            setTimeout(() => {
+                updateDuration(start - performance.now());
+                updateGame();
+            }, 0);
             // }
         }
 
         const start = performance.now();
-        setTimeout(updateEpisode, 0);
+        setTimeout(() => {
+            updateDuration(start - performance.now());
+            updateEpisode();
+        }, 0);
     });
 })();
 
