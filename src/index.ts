eae8b712941973bbba7e4eb6426496f5d62c048f
CJ Dimaano <c.j.s.dimaano@gmail.com>
2019-09-10 10:38:57 -0700

    add state info to dom

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,36 @@
 *******************************************************************************/
 
 import Game from "./game";
-import * as moment from "moment";
+import * as numeral from "numeral";
 
-window.addEventListener("load", () => {
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    const game = new Game(canvas.getContext("2d")!);
-    const durationElement = document.getElementById("duration")!;
-    let prevTick = performance.now();
-    const start = prevTick;
-    const animation = (now: DOMHighResTimeStamp) => {
-        durationElement.textContent = moment.duration(start - now).humanize();
-        game.update((now - prevTick) / 1000.0);
-        game.draw();
-        prevTick = now;
+(() => {
+    window.addEventListener("load", () => {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        const game = new Game(canvas.getContext("2d")!);
+
+        const durationElement = document.getElementById("duration")!;
+        const trState = document.getElementById("state")!;
+
+        const tdChoices = Array<HTMLElement>(6);
+        for (let i = 0; i < tdChoices.length; i++)
+            tdChoices[i] = document.getElementById(`choice${i}`)!;
+
+        let prevTick = performance.now();
+        const start = prevTick;
+        const animation = (now: DOMHighResTimeStamp) => {
+            durationElement.textContent = numeral((now - start) / 1000.0).format("00:00:00");
+            game.update((now - prevTick) / 1000.0);
+            game.draw();
+            for (let i = 0; i < trState.children.length; i++) {
+                trState.children[i].textContent = numeral(game.agentAi.mem[0][i]).format("0.000");
+            }
+            for (let i = 0; i < tdChoices.length; i++) {
+                tdChoices[i].children[0].textContent = numeral(game.agentAi.mem[1][i]).format("0.00000");
+                tdChoices[i].children[1].textContent = numeral(game.agentAi.mem[2][i]).format("0.00000");
+            }
+            prevTick = now;
+            requestAnimationFrame(animation);
+        };
         requestAnimationFrame(animation);
-    };
-    requestAnimationFrame(animation);
-});
+    });
+})();
